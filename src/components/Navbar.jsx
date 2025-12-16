46439f96b47f606beeef3c1bca38f8b3d99e7b5d
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-12-16 15:34:42 -0300

    Navbar Mobile
    Criando a navbar para o mobile e dando alguns ajustes na navbar de dektop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import '../style/Navbar.css'
 import logo from '/image/logo.svg';
+import { NavLink } from "react-router-dom";
+
+
+const pageTitles = {
+    dashboard: 'Dashboard',
+    pacientes: 'Pacientes',
+    prontuarios: 'Prontuários',
+    relatorios: 'Relatórios',
+    remedios: 'Remédios',
+};
+
 
 const navItems = [
     {
@@ -33,76 +44,151 @@ const navItems = [
 
 function Navbar() {
 
-    // function fnNomeUsuario() {
-    //     let nomeUsuario = prompt("Qual seu nome?");
-    //     document.getElementById("nomeUsuario").innerHTML = nomeUsuario;
-    // }
 
     return (
-        <nav className="navbar navbar-system mt-3 rounded-4 position-relative bg-body-tertiary">
+        <>
+            {/* Navbar Desktop */}
 
-            {/* Logo à esquerda */}
-            <div className="d-flex align-items-center ms-4">
-                <a
-                    href="#"
-                    className="navbar-brand d-flex align-items-center"
-                >
-                    <img
-                        src={logo}
-                        alt="Logo Sistema Senac"
-                        className="img-fluid object-fit-contain"
-                        style={{ height: 40 }}
-                    />
+            <nav className="navbar navbar-system mt-3 rounded-4 position-relative bg-body-tertiary d-none d-xl-flex">
 
-                    <div className="ms-3">
-                        <h1 className="mb-0 fs-6 fw-semibold">
-                            Prontuário Eletrônico
-                        </h1>
-                        <p className="mb-0 fs-6 text-muted">
-                            Sistema Senac
-                        </p>
-                    </div>
-                </a>
-            </div>
-
-
-            {/* Botões centralizados */}
-            <div className="position-absolute top-50 start-50 translate-middle d-flex gap-5">
-                {navItems.map((item) => (
-                    <button
-                        key={item.route}
-                        className="btn text-decoration-none d-flex align-items-center gap-1 p-0 text-dark btn-link"
-                        onClick={() => console.log(item.route)}
+                {/* Logo à esquerda */}
+                <div className="d-flex align-items-center ms-4">
+                    <a
+                        href="#"
+                        className="navbar-brand d-flex align-items-center"
                     >
-                        <i className={`${item.iconClass} fs-6 me-1 text-dark`} />
-                        <span className="fs-6">{item.label}</span>
-                    </button>
-                ))}
-            </div>
-            {/* Botões à direita */}
-            <div className="d-flex align-items-center me-4">
+                        <img
+                            src={logo}
+                            alt="Logo Sistema Senac"
+                            className="img-fluid object-fit-contain"
+                            style={{ height: 40 }}
+                        />
 
-                <div className="me-4">
-                    <h1 className="mb-0 fs-6 fw-semibold">Eduardo Beretta</h1>
-                    <p className="mb-0 small text-muted text-end">Docente</p>
+                        <div className="ms-3">
+                            <h1 className="mb-0 fs-6 fw-semibold">
+                                Prontuário Eletrônico
+                            </h1>
+                            <p className="mb-0 fs-6 text-muted">
+                                Sistema Senac
+                            </p>
+                        </div>
+                    </a>
                 </div>
-                {/* Botão de perfil */}
-                <button href='' className="btn btn-link icon-button shadow-none focus-ring-0 p-0 me-3">
-                    <i className='bi bi-person fs-6 mx-auto'></i>
-                </button>
-                {/* Botão de configuração */}
-                <a
-                    href=''
-                    className="btn btn-link icon-confi shadow-none focus-ring-0 p-0"
-                >
-                    <i className="bi bi-gear fs-6 mx-auto"></i>
-                </a>
 
 
-            </div>
-        </nav>
+                {/* Botões centralizados */}
+                <div className="position-absolute top-50 start-50 translate-middle d-flex container-button-navbar">
+                    {navItems.map((item) => (
+                        <NavLink
+                            key={item.route}
+                            to={`/${item.route}`}
+                            className={({ isActive }) =>
+                                `btn btn-link text-decoration-none d-flex align-items-center gap-1 p-0
+     ${isActive ? 'nav-active text-primary fw-semibold' : 'text-dark'}`}>
+                            <i className={`${item.iconClass} fs-6 me-1`} />
+                            <span className="fs-6">{item.label}</span>
+                        </NavLink>
 
+                    ))}
+                </div>
+                {/* Botões à direita */}
+                <div className="d-flex align-items-center me-4">
 
+                    <div className="me-4">
+                        <h1 className="mb-0 fs-6 fw-semibold">Eduardo Beretta</h1>
+                        <p className="mb-0 small text-muted text-end">Docente</p>
+                    </div>
+                    {/* Botão de perfil */}
+                    <button href='' className="btn btn-link icon-button shadow-none focus-ring-0 p-0 me-3">
+                        <i className='bi bi-person fs-6 mx-auto'></i>
+                    </button>
+                    {/* Botão de configuração */}
+                    <a
+                        href=''
+                        className="btn btn-link icon-confi shadow-none focus-ring-0 p-0">
+                        <i className="bi bi-gear fs-6 mx-auto"></i>
+                    </a>
+                </div>
+            </nav>
+
+            {/* Navbar Mobile */}
+
+            <nav className="navbar fixed-top d-flex d-xl-none">
+                <div className="container-fluid">
+
+                    {/* Botões da Esquerda */}
+                    <div className="d-flex align-items-center ms-4">
+                        <a
+                            href="#"
+                            className="navbar-brand d-flex align-items-center"
+                        >
+                            <img
+                                src={logo}
+                                alt="Logo Sistema Senac"
+                                className="img-fluid object-fit-contain"
+                                style={{ height: 40 }}
+                            />
+
+                            <div className="ms-3">
+                                <h1 className="mb-0 fs-6 fw-semibold">
+                                    Prontuário Eletrônico
+                                </h1>
+                                <p className="mb-0 fs-6 text-muted">
+                                    Sistema Senac
+                                </p>
+                            </div>
+                        </a>
+                    </div>
+
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="offcanvas offcanvas-end" tabIndex={"-1"} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+
+                            {/* Botões do header da Navbar */}
+                            <div className="d-flex align-items-center mt-3">
+
+                                {/* Botão de perfil */}
+                                <button href='' className="btn btn-link icon-button shadow-none focus-ring-0 p-0 ms-2 me-3">
+                                    <i className='bi bi-person fs-6 mx-auto'></i>
+                                </button>
+                                <div className="me-4">
+                                    <h1 className="mb-0 fs-6 fw-semibold">Eduardo Beretta</h1>
+                                    <p className="mb-0 small text-muted text-start">Docente</p>
+                                </div>
+                            </div>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <hr class="my-3 border-secondary opacity-25" />
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav">
+                                {navItems.map(item => (
+                                    <li key={item.route} className="nav-item ms-2 btn p-lg-1 btn-slide-navbar d-flex mb-3 rounded-4">
+                                        <NavLink
+                                            to={`/${item.route}`}
+                                            className="nav-link ms-2"
+                                            data-bs-dismiss="offcanvas">
+                                            <i className={`${item.iconClass} fs-6 me-3`} />
+                                            {item.label}
+                                        </NavLink>
+                                    </li>
+                                ))}
+                            </ul>
+                            <hr class="my-3 border-secondary opacity-25" />
+                        </div>
+
+                        {/* Botão de configuração */}
+                        <a
+                            href=''
+                            className="btn btn-link icon-confi shadow-none focus-ring-0 p-0 position-absolute bottom-0 end-0 mb-4 me-4">
+                            <i className="bi bi-gear fs-6 mx-auto"></i>
+                        </a>
+                    </div>
+                </div>
+            </nav>
+
+        </>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
