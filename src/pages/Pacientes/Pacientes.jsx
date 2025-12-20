5261ec3225dce5f588728d00b5a308c7fdc3c02f
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-12-20 15:00:54 -0300

    Modal
    Criando o modal de 'Novo Paciente'. Criei todo o html e css do modal, foi feito em sua maioria com o bootstrap, faltam os IDs e o js para funcionar

--- src/pages/Pacientes/Pacientes.jsx
+++ src/pages/Pacientes/Pacientes.jsx
@@ -6,6 +6,111 @@ const Pacientes = () => {
     return (
         <section id='pacientes-page-section'>
 
+
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="modalCriarPaciente" tabIndex="-1" aria-hidden='true'>
+                <div className="modal-dialog modal-dialog-centered modal-lg">
+                    <div className="modal-content">
+
+                        <div className="modal-header">
+                            <div className='p-2'>
+                            <h5 className="modal-title">Novo Paciente</h5>
+                            <p className='small opacity-75'>Preencha os dados do paciente para fins educacionais</p>
+                            </div>
+                            <button
+                                type="button"
+                                className="btn-close mb-5"
+                                data-bs-dismiss="modal"
+                            ></button>
+                        </div>
+
+                        <div className="modal-body">
+                            <form className='row g-3'>
+
+                                <div className='col-12'>
+                                    <label className='form-label'>Nome do Paciente *</label>
+                                    <input type="text" className='form-control' />
+                                </div>
+
+                                <div className='col-12'>
+                                    <label className='form-label'>Nome da Mãe *</label>
+                                    <input type="text" className='form-control' />
+                                </div>
+
+                                <div className='col-md-6'>
+                                    <label className='form-label'>Data de Nascimento *</label>
+                                    <input type="date" className='form-control' />
+                                </div>
+
+                                <div className='col-md-4'>
+                                    <label className='form-label'>Tipo Sanguíneo *</label>
+                                    <select className='form-select'>
+                                        <option value="A">A</option>
+                                        <option value="B">B</option>
+                                        <option value="AB">AB</option>
+                                        <option value="O">O</option>
+                                    </select>
+                                </div>
+
+                                <div className='col-md-2 mt-5'>
+                                    <select className='form-select'>
+                                        <option value="+">+</option>
+                                        <option value="-">-</option>
+                                    </select>
+                                </div>
+
+                                <div className='col-12'>
+                                    <label className='form-label'>Equipe *</label>
+                                    <input type="text" className='form-control' placeholder='Ex: Equipe Azul - Clínica Médica' />
+                                    <p className='small mt-1 opacity-50'>A equipe representa o grupo multiprofissional responsável pelo cuidado ao paciente</p>
+                                </div>
+
+                                <div className='col-md-6'>
+                                    <label className='form-label'>Status do Paciente *</label>
+                                    <select className='form-select'>
+                                        <option value="estavel">Estável</option>
+                                        <option value="observacao">Em Observação</option>
+                                        <option value="critico">Crítico</option>
+                                    </select>
+                                    <p className='small mt-1 opacity-50'>Define o nível de atenção necessário para o paciente</p>
+                                </div>
+                                
+                                <div className='col-md-6'>
+                                    <label className='form-label'>Convênio *</label>
+                                    <input type="text" className='form-control' 
+                                    placeholder='Ex: SUS, Unimed, etc.'/>
+                                </div>
+
+                                <div className='col-md-3'>
+                                    <label className='form-label'>Quarto *</label>
+                                    <input type="text" className='form-control' 
+                                    placeholder='Ex: 201'/>
+                                </div>
+
+                                <div className='col-md-3'>
+                                    <label className='form-label'>Leito *</label>
+                                    <input type="text" className='form-control' placeholder='Ex: A' />
+                                </div>
+
+                            </form>
+                        </div>
+
+                        <div className="modal-footer">
+                            <button
+                                className="btn btn-outline-danger"
+                                data-bs-dismiss="modal"
+                            >
+                                Cancelar
+                            </button>
+                            <button className="btn btn-primary">
+                                Criar Paciente
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
             <div className='container-pacientes'>
 
                 <div className="d-flex flex-column flex-md-row mb-3 align-items-start align-items-md-center justify-content-md-between">
@@ -16,7 +121,7 @@ const Pacientes = () => {
                     </div>
 
                     <div className="d-flex justify-content-md-end container-action-btn">
-                        <button className="btn btn-primary d-flex align-items-center gap-2 header-action-btn">
+                        <button className="btn btn-primary d-flex align-items-center gap-2 header-action-btn" data-bs-toggle="modal" data-bs-target='#modalCriarPaciente'>
                             <i className="bi bi-plus fs-5"></i>
                             Novo Paciente
                         </button>
@@ -24,19 +129,19 @@ const Pacientes = () => {
                 </div>
 
                 <div>
-                    <form class="d-flex flex-column flex-md-row gap-3" role="search">
+                    <form className="d-flex flex-column flex-md-row gap-3" role="search">
 
-                        <div class="position-relative w-100 d-flex">
-                            <i class="bi bi-search position-absolute top-50 translate-middle-y ms-3 text-secondary"></i>
+                        <div className="position-relative w-100 d-flex">
+                            <i className="bi bi-search position-absolute top-50 translate-middle-y ms-3 text-secondary"></i>
                             <input
                                 type="text"
-                                class="form-control input-search"
+                                className="form-control input-search"
                                 placeholder="Buscar por nome, equipe ou quarto..."
                             />
                         </div>
 
-                        <button class="btn-filter" type="submit">
-                            <i class="bi bi-funnel fs-5"></i> Filtros
+                        <button className="btn-filter" type="button">
+                            <i className="bi bi-funnel fs-5"></i> Filtros
                         </button>
                     </form>
 
