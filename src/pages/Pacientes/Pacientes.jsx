5c55c647615bf5291fb07a4a9a98a73f2d9fb8e0
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-12-20 22:18:46 -0300

    Pacientes
    fazendo o collapse do botao de filtros

--- src/pages/Pacientes/Pacientes.jsx
+++ src/pages/Pacientes/Pacientes.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import { useState } from "react";
 import './Pacientes.css'
 import TagStatus from '../../components/Tag/TagStatus'
 
 const Pacientes = () => {
+
+    const [ativo, setAtivo] = useState(false);
+
     return (
         <section id='pacientes-page-section'>
 
 
-            {/* <!-- Modal --> */}
+            {/* Modal */}
             <div className="modal fade" id="modalCriarPaciente" tabIndex="-1" aria-hidden='true'>
                 <div className="modal-dialog modal-dialog-centered modal-lg">
                     <div className="modal-content">
 
                         <div className="modal-header">
                             <div className='p-2'>
-                            <h5 className="modal-title">Novo Paciente</h5>
-                            <p className='small opacity-75'>Preencha os dados do paciente para fins educacionais</p>
+                                <h5 className="modal-title">Novo Paciente</h5>
+                                <p className='small opacity-75'>Preencha os dados do paciente para fins educacionais</p>
                             </div>
                             <button
                                 type="button"
@@ -45,18 +49,18 @@ const Pacientes = () => {
                                 <div className='col-md-4'>
                                     <label className='form-label'>Tipo Sanguíneo *</label>
                                     <select className='form-select'>
-                                        <option value="A">A</option>
-                                        <option value="B">B</option>
-                                        <option value="AB">AB</option>
-                                        <option value="O">O</option>
+                                        <option value={'A'}>A</option>
+                                        <option value={'B'}>B</option>
+                                        <option value={'AB'}>AB</option>
+                                        <option value={'O'}>O</option>
                                     </select>
                                 </div>
 
                                 <div className='col-md-2'>
                                     <label className='form-label'>Fator RH *</label>
                                     <select className='form-select'>
-                                        <option value="+">+</option>
-                                        <option value="-">-</option>
+                                        <option value={'-'}>+</option>
+                                        <option value={'-'}>-</option>
                                     </select>
                                 </div>
 
@@ -75,17 +79,17 @@ const Pacientes = () => {
                                     </select>
                                     <p className='small mt-1 opacity-50'>Define o nível de atenção necessário para o paciente</p>
                                 </div>
-                                
+
                                 <div className='col-md-6'>
                                     <label className='form-label'>Convênio *</label>
-                                    <input type="text" className='form-control' 
-                                    placeholder='Ex: SUS, Unimed, etc.'/>
+                                    <input type="text" className='form-control'
+                                        placeholder='Ex: SUS, Unimed, etc.' />
                                 </div>
 
                                 <div className='col-md-3'>
                                     <label className='form-label'>Quarto *</label>
-                                    <input type="text" className='form-control' 
-                                    placeholder='Ex: 201'/>
+                                    <input type="text" className='form-control'
+                                        placeholder='Ex: 201' />
                                 </div>
 
                                 <div className='col-md-3'>
@@ -111,6 +115,7 @@ const Pacientes = () => {
                     </div>
                 </div>
             </div>
+
 
             <div className='container-pacientes'>
 
@@ -141,10 +146,62 @@ const Pacientes = () => {
                             />
                         </div>
 
-                        <button className="btn-filter" type="button">
-                            <i className="bi bi-funnel fs-5"></i> Filtros
+                        <button
+                            type="button"
+                            className={`btn btn-filter ${ativo ? "bg-primary text-white" : "btn-outline-primary text-dark"
+                                }`}
+                            onClick={() => setAtivo(!ativo)}
+                            data-bs-toggle="collapse"
+                            data-bs-target="#FormularioFiltro"
+                            aria-expanded={ativo}
+                            aria-controls="FormularioFiltro"
+                        >
+                            <i className="bi bi-funnel fs-5 me-1"></i>
+                            Filtros
                         </button>
+
+
+
                     </form>
+
+                    {/* Collapse */}
+                    <div class="collapse" id="FormularioFiltro">
+                        <form className='row g-3 form-control d-flex mt-3 m-0'>
+
+                            <div className='col-md-4'>
+                                <label className='form-label'>Status do Paciente</label>
+                                <select className='form-select'>
+                                    <option disabled>Todos os status</option>
+                                    <option value={'estavel'}>Estável</option>
+                                    <option value={'observacao'}>Em Observação</option>
+                                    <option value={'critico'}>Crítico</option>
+                                </select>
+                            </div>
+
+                            <div className='col-md-4'>
+                                <label className='form-label'>Equipe</label>
+                                <select className='form-select'>
+                                    <option disabled>Todas as Equipes</option>
+                                    <option value={'ClinicaMedica'}>Equipe Azul - Clínica Médica</option>
+                                    <option value={'Cardologia'}>Equipe Verde - Cardologia</option>
+                                    <option value={'UTI'}>Equipe Vermelha - UTI</option>
+                                </select>
+                            </div>
+
+                            <div className='col-md-4'>
+                                <label className='form-label'>Convênio</label>
+                                <select className='form-select'>
+                                    <option disabled>Todos os convênios</option>
+                                    <option value={'SUS'}>SUS</option>
+                                    <option value={'Unimed'}>Unimed</option>
+                                    <option value={'Particular'}>Particular</option>
+                                </select>
+                            </div>
+                            <button type='button' className=' btn-Limparfiltro text-primary w-auto mt-3 mb-2'>
+                                Limpar filtros
+                            </button>
+                        </form>
+                    </div>
 
                 </div>
 
